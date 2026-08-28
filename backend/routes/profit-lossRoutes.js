9af78fa856c8dfec0a09b39d1d92d3e1b314ea84
santhiyaganeshkumar <santhiyaganeshkumar759@gmail.com>
2026-08-28 12:31:21 +0530

    acc

--- backend/routes/profit-lossRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const profitLossController = require("../controllers/profit-lossController");
-
-router.get("/", profitLossController.getProfitLoss);
-
-module.exports = router;

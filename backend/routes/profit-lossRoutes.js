8ae8d285bf5eff380449152dc283be077fed55a2
Kaarunya <kaarunyabs2005@gmail.com>
2026-08-28 12:30:49 +0530

    acc

--- backend/routes/profit-lossRoutes.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const router=express.Router();
-
-const profitLossController = require("../controllers/profitLossController");
-
-router.get("/", profitLossController.getProfitLoss);
-
-module.exports = router;
